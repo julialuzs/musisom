0ab033270a395ac8fd884d823d5f9d6ea0737dff
alunoinfo <alunoinfo@ifsul-sapucaia.local>
2019-10-15 10:35:14 -0300

    Adicionando form de cadastro

---
+++ cadastrar-form.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+	<title>Cadastro de Cliente</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+<body>
+	<div class='container'>
+		<fieldset>
+			
+			<form action="action_cliente.php" method="post" id='form-contato' enctype='multipart/form-data'>
+				<div class="row">
+					<label for="nome">Selecionar Foto</label>
+			      	<div class="col-md-2">
+			
+					<img src="imagens/guitarra.png" id="foto-cliente">
+
+				  	</div>
+				  	<input type="file" name="foto" id="foto" value="foto" >
+			  	</div>
+
+			    <div class="form-group">
+			      <label for="descricao">Descrição</label>
+			      <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Infome a descrição">
+			      <span class='msg-erro msg-descricao'></span>
+			    </div>
+
+			    <div class="form-group">
+			      <label for="valor">Preço</label>
+			      <input type="valor" class="form-valor" id="valor" maxlength="12" name="valor" placeholder="Informe o valor">
+			      <span class='msg-erro msg-valor'></span>
+			    </div>
+			  
+			    <div class="form-group">
+			      <label for="tipo">Tipo</label>
+			      <select class="form-control" name="tipo" id="tipo">
+				    <option value="">Selecione o Tipo</option>
+				    <option value="cordofone">Cordofone</option>
+                    <option value="aerofone">Aerofone</option>
+                    <option value="idiofone">Idiofone</option>
+                    <option value="eletrofone">Eletrofone</option>
+				  </select>
+				  <span class='msg-erro msg-tipo'></span>
+			    </div>
+
+			    <input type="hidden" name="acao" value="incluir">
+			    <button type="submit" class="btn btn-primary" id='botao'> 
+			      Gravar
+			    </button>
+			    <a href='index.php' class="btn btn-danger">Cancelar</a>
+			</form>
+		</fieldset>
+	</div>
+	<script type="text/javascript" src="js/custom.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -7,9 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css">
-    <link rel="stylesheet" type="text/css" href="css/template.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css">
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>MUSISOM</title>
@@ -19,28 +19,32 @@
 
     <div id="container">
 
-        <header>
-            <div>MUSISOM</div>
-        </header>
-        <div id="menu">
-            <a id="menu-home" class="nav-item nav-link active">HOME</a>
-            <a id="menu-cadastrar" class="nav-item nav-link">CADASTRAR</a>
-            <a id="menu-editar">EDITAR</a>
-            <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <span id="title"> MUSISOM</span>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#"> Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="cadastrar-form.php"> Cadastrar </a>
+                    </li>
 
-                <div id="conteudo">
-
-                    <nav class="navbar navbar-light bg-light">
-                        <form class="form-inline">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                        </form>
-                    </nav>
-
-                </div>
-
+                </ul>
             </div>
+        </nav>
 
+        <div id="conteudo">
+
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+
+        </div>
+
+    </div> <!--fim container-->
 
 </body>
 
