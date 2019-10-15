286b0c2094960c6dc9c4bf40d2305c0bb7ba3d2a
alunoinfo <alunoinfo@ifsul-sapucaia.local>
2019-10-15 08:46:06 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css">
+    <link rel="stylesheet" type="text/css" href="css/template.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>MUSISOM</title>
+</head>
+
+<body>
+
+    <div id="container">
+
+        <header>
+            <div>MUSISOM</div>
+        </header>
+        <div id="menu">
+            <a id="menu-home" class="nav-item nav-link active">HOME</a>
+            <a id="menu-cadastrar" class="nav-item nav-link">CADASTRAR</a>
+            <a id="menu-editar">EDITAR</a>
+            <div>
+
+                <div id="conteudo">
+
+                    <nav class="navbar navbar-light bg-light">
+                        <form class="form-inline">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </nav>
+
+                </div>
+
+            </div>
+
+
+</body>
+
+</html>
