0eccf83f573c5f2d21ba77e6566af7b93cd67833
Julia Luz Maciel dos Santos <julia.15081@gmail.com>
2019-10-28 23:03:27 -0300

    get produtos na home

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
+<?php
+require 'conexao.php';
+
+// Recebe o termo de pesquisa se existir
+$termo = (isset($_GET['termo'])) ? $_GET['termo'] : '';
+
+// Verifica se o termo de pesquisa está vazio, se estiver executa uma consulta completa
+if (empty($termo)) :
+
+    $conexao = conexao::getInstance();
+    $sql = 'SELECT codigo, tipo, marca, descricao, valor, qtd_estoque FROM produtos';
+    $stm = $conexao->prepare($sql);
+    $stm->execute();
+    $produtos = $stm->fetchAll(PDO::FETCH_OBJ);
+else :
+    // Executa uma consulta baseada no termo de pesquisa passado como parâmetro
+    $conexao = conexao::getInstance();
+    $sql = 'SELECT codigo, tipo, marca, descricao, valor, qtd_estoque FROM produtos WHERE descricao LIKE :descricao OR tipo LIKE :tipo OR marca LIKE :marca';
+    $stm = $conexao->prepare($sql);
+    $stm->bindValue(':descricao', $termo . '%');
+    $stm->bindValue(':tipo', $termo . '%');
+    $stm->bindValue(':marca', $termo . '%');
+    $stm->execute();
+    $produtos = $stm->fetchAll(PDO::FETCH_OBJ);
+
+endif;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -30,27 +58,27 @@
 
             <?php if (!empty($produtos)) : ?>
 
-                <!-- Tabela de Clientes -->
                 <table class="table table-striped">
                     <tr class='active'>
                         <th>Foto</th>
-                        <th>Descrição</th>
                         <th>Tipo</th>
+                        <th>Descrição</th>
                         <th>Marca</th>
                         <th>Valor</th>
                         <th>Quantidade em Estoque</th>
                         <th>Ação</th>
                     </tr>
-                    <?php foreach ($clientes as $cliente) : ?>
+                    <?php foreach ($produtos as $produto) : ?>
                         <tr>
-                            <td><img src='fotos/<?= $cliente->foto ?>' height='40' width='40'></td>
-                            <td><?= $cliente->nome ?></td>
-                            <td><?= $cliente->email ?></td>
-                            <td><?= $cliente->celular ?></td>
-                            <td><?= $cliente->status ?></td>
+                            <td><img src='fotos/<?= $produto->foto ?>' height='40' width='40'></td>
+                            <td><?= $produto->tipo ?></td>
+                            <td><?= $produto->descricao ?></td>
+                            <td><?= $produto->marca ?></td>
+                            <td><?= $produto->valor ?></td>
+                            <td><?= $produto->qtd_estoque ?></td>
                             <td>
-                                <a href='editar.php?id=<?= $cliente->id ?>' class="btn btn-primary">Editar</a>
-                                <a href='javascript:void(0)' class="btn btn-danger link_exclusao" rel="<?= $cliente->id ?>">Excluir</a>
+                                <a href='editar.php?codigo=<?= $produto->codigo ?>' class="btn btn-primary">Editar</a>
+                                <a href='javascript:void(0)' class="btn btn-danger link_exclusao" rel="<?= $produto->codigo ?>">Excluir</a>
                             </td>
                         </tr>
                     <?php endforeach; ?>
@@ -61,8 +89,9 @@
             <?php endif; ?>
 
         </div>
-
     </div>
+
+    <script type="text/javascript" src="js/custom.js"></script>
 
 </body>
 
